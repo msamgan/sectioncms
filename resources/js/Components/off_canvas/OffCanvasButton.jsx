1204ee0d61af73f0c908dab986d1e2f6b3f6202b
msamgan <mohdsamgankhan@gmail.com>
2025-05-19 12:16:49 -0400

    Update

--- resources/js/Components/off_canvas/OffCanvasButton.jsx
+++ resources/js/Components/off_canvas/OffCanvasButton.jsx
@@ -1,14 +1,15 @@
-export default function OffCanvasButton({ id, children, className = 'btn btn-primary', ...props }) {
+export default function OffCanvasButton({ id, children, icon = 'ri-menu-line', className = '', ...props }) {
     return (
-        <span
-            className={className}
+        <button
+            className={`btn btn-primary shadow-sm hover:shadow-md transition-all duration-200 d-inline-flex align-items-center ${className}`}
             type="button"
             data-bs-toggle="offcanvas"
             data-bs-target={'#' + id}
             aria-controls={id}
             {...props}
         >
+            {icon && <i className={`${icon} me-2`}></i>}
             {children}
-        </span>
+        </button>
     )
 }
