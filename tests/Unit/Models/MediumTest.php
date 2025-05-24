c4cc1f08495838c72d48037b4a967c9d3c9c8a89
msamgan <mohdsamgankhan@gmail.com>
2025-05-24 19:50:48 -0400

    Format.

--- tests/Unit/Models/MediumTest.php
+++ tests/Unit/Models/MediumTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Actions\Business\CreateBusiness;
 use App\Actions\Role\AssignRole;
 use App\Models\Medium;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -13,7 +14,7 @@ uses(TestCase::class, RefreshDatabase::class);
 
 test('can create a medium using factory', function (): void {
     $user = User::factory()->create();
-    (new AssignRole())->handle(user: $user, role: App\Models\Role::business(), makeRoleActive: true);
+    (new AssignRole())->handle(user: $user, role: Role::business(), makeRoleActive: true);
     (new CreateBusiness())->handle(user: $user, businessName: 'laravel.com', makeBusinessActive: true);
 
     $medium = Medium::factory()->create([
@@ -26,7 +27,7 @@ test('can create a medium using factory', function (): void {
 
 test('medium logs activity', function (): void {
     $user = User::factory()->create();
-    (new AssignRole())->handle(user: $user, role: App\Models\Role::business(), makeRoleActive: true);
+    (new AssignRole())->handle(user: $user, role: Role::business(), makeRoleActive: true);
     (new CreateBusiness())->handle(user: $user, businessName: 'laravel.com', makeBusinessActive: true);
 
     $medium = Medium::factory()->create([
