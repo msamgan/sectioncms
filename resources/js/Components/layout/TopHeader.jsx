b44adffa73448f1c06408c8d4e2f8451e6fc2326
msamgan <mohdsamgankhan@gmail.com>
2025-05-25 16:00:47 -0400

    Update

--- resources/js/Components/layout/TopHeader.jsx
+++ resources/js/Components/layout/TopHeader.jsx
@@ -3,7 +3,7 @@ import TopHeaderRight from '@/Components/layout/TopHeaderRight.jsx'
 
 export default function TopHeader({ user }) {
     return (
-        <nav className="flex items-center bg-white shadow" id="layout-navbar">
+        <nav className="flex items-center bg-white" id="layout-navbar">
             <div className="container mx-auto px-4 flex justify-between py-2">
                 <div className="flex items-center">
                     <div className="hidden xl:flex items-center py-0">
